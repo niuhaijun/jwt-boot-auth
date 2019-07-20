5549a18200ed1ea7fd3ba49cf606cf19bbd58a45
牛海军 <niuhaijun@100tal.com>
2019-07-20 15:29:59 +0800

    format code

--- src/main/java/com/example/jwtauth/JwtAuthenticationFilter.java
+++ src/main/java/com/example/jwtauth/JwtAuthenticationFilter.java
@@ -1,17 +1,16 @@
 package com.example.jwtauth;
 
 import io.jsonwebtoken.Jwts;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-
+import java.io.IOException;
+import java.util.ArrayList;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.ArrayList;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 public class JwtAuthenticationFilter extends BasicAuthenticationFilter {
 
@@ -22,8 +21,7 @@ public class JwtAuthenticationFilter extends BasicAuthenticationFilter {
 
   @Override
   protected void doFilterInternal(HttpServletRequest req,
-      HttpServletResponse res,
-      FilterChain chain) throws IOException, ServletException {
+      HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
 
     String header = req.getHeader("Authorization");
 
--- src/main/java/com/example/jwtauth/MyWebSecurityConfig.java
+++ src/main/java/com/example/jwtauth/MyWebSecurityConfig.java
@@ -6,7 +6,6 @@ import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
